2f00bff23474922a07a645379175201c5ee25044
hoangthai2171 <hoangthai2171@gmail.com>
2018-04-17 20:35:01 +0700

    Tuan 6

---
+++ Tuan 2.1/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Tuan_2._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Client IP :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(249, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 48);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(322, 160);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Message :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 251);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(322, 70);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(404, 251);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(144, 70);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Send";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 343);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Tuan 2.1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+namespace Tuan_2._1
+{
+    public partial class Form1 : Form
+    {
+        Socket newsock;
+        Socket client;
+        IPEndPoint ipe;
+        int connections = 0;
+        bool enter = true;
+        public Form1()
+        {
+            InitializeComponent();
+            ipe = new IPEndPoint(IPAddress.Any, 9050);
+            newsock = new Socket(AddressFamily.InterNetwork,
+                            SocketType.Stream, ProtocolType.Tcp);
+            newsock.Bind(ipe);
+            newsock.Listen(10);
+            listBox1.Items.Add("Waiting for clients...");
+            client = newsock.Accept();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+                byte[] data = new byte[1024];
+                data = Encoding.ASCII.GetBytes(textBox2.Text);
+                client.Send(data,data.Length,SocketFlags.None);
+                listBox1.Items.Add(textBox2.Text.ToString());
+                        Thread newthread = new Thread(new ThreadStart(HandleConnection));
+                        newthread.Start();
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void HandleConnection()
+        {
+            while (true)
+            {
+                byte[] data = new byte[1024];
+                int recv = client.Receive(data, SocketFlags.None);
+                string stringData = Encoding.ASCII.GetString(data, 0, recv);
+                listBox1.Items.Add(stringData.Substring(0, recv));
+            }
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        
+       {
+            
+            if (e.KeyCode == Keys.Enter)
+            {
+                byte[] data = new byte[1024];
+                data = Encoding.ASCII.GetBytes(textBox2.Text);
+                client.Send(data, data.Length, SocketFlags.None);
+                listBox1.Items.Add(textBox2.Text.ToString());
+                Thread newthread = new Thread(new ThreadStart(HandleConnection));
+                newthread.Start();
+                textBox2.Clear();
+                SendKeys.Send(textBox2.Text);
+                e.SuppressKeyPress = true;         
+            }
+            
+            
+        }
+    }
+}
